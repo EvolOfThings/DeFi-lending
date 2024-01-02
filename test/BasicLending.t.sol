1fb882dd46fa9dd7eb4573f824a617fd0a83117c
rishad <rishad.yamnoor@gmail.com>
2024-01-02 19:42:22 +0530

    script

---
+++ test/BasicLending.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
+
+// import {Test, console2} from "forge-std/Test.sol";
+// import {BasicLending} from "../src/BasicLending.sol";
+
+// contract LendingTest is Test {
+//     BasicLending public counter;
+
+
+// }
